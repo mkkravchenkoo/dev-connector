7852df7d3da83f7f54006bf60e634e7b939ce625
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-21 00:00:23 +0300

    get authenticated user from jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,21 @@
 const express = require('express')
 
 const router = express.Router();
-
+const User = require('../../models/User')
+const auth = require('../../middleware/auth');
 
 // @route GET 	api/auth
 // @desc Test 	route
 // @access 		Public
-router.get('/', (req, res) => res.send('auth route'))
+router.get('/', auth, async (req, res) => {
+
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	}catch (e) {
+		res.status(500).send('Server error');
+	}
+
+})
 
 module.exports = router
