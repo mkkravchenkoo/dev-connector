8ed7270e22841aef6976c2d33eaaceeb1c4e3639
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-20 22:14:05 +0300

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express')
-
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB()
 
 app.get('/', (req, res) => {
 	res.send('API running')
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
