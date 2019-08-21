5c60054d0559ae1e7a025ef59468ee8968cd419e
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-21 22:24:13 +0300

    fixed mongo warning "useFindAndModify"

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
 	try {
-		await  mongoose.connect(db, { useNewUrlParser: true,  useCreateIndex:true})
+		await mongoose.connect(db, {
+				useNewUrlParser: true,
+				useCreateIndex: true, useFindAndModify: false
+			}
+		)
 		console.log('mongo DB connected...')
 	}catch (err) {
 		console.log(err.message);
