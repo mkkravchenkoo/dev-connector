232bde00123959682ca6e93f985a1cc3aab53b71
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-22 00:04:19 +0300

    use request module, connected to remote api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 
 const router = express.Router();
 const auth = require('../../middleware/auth');
@@ -309,6 +311,45 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 
 		await profile.save();
 		res.json(profile);
+
+	}catch (e) {
+		console.log(e)
+		res.status(500).send("Server error")
+	}
+});
+
+
+// @route GET  api/profile/github/:username
+// @desc       get user repos from github
+// @access     Public
+
+
+router.get('/github/:username', async (req, res) => {
+
+	try {
+		const options = {
+			uri: `https://api.github.com/users/${
+				req.params.username
+				}/repos?per_page=5&sort=created:asc&client_id=${config.get(
+				'githubClientId'
+			)}&client_secret=${config.get('githubSecret')}`,
+			method: 'GET',
+			headers: { 'user-agent': 'node.js' }
+		};
+
+
+
+		request(options, (error, response, body) => {
+			if(error){
+				console.log(error)
+			}
+
+			if(response.statusCode !== 200){
+				return res.status(404).json({msg:"No github profile"})
+			}
+
+			res.json(JSON.parse(body))
+		});
 
 	}catch (e) {
 		console.log(e)
