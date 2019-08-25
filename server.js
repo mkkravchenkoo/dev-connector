8b2dfd716b6ac3eb60940e2e2a6ee41591661f09
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-08-25 20:14:31 +0300

    CORS

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 app.use(express.json({extended:false})); // need for getting request in json
 
 // define routes
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
