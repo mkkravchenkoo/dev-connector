30b30854f5f55dd4e585710319b46f3fec0ff753
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-09-01 21:01:29 +0300

    Create profile logic

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import {createProfile} from '../../actions/profile'
 
-const CreateProfile = (props) => {
+const CreateProfile = ({createProfile, history}) => {
 	const [formData, setFormData] = useState({
 		company: "",
 		website: "",
@@ -40,6 +41,11 @@ const CreateProfile = (props) => {
 		})
 	}
 
+	const onSubmit = (e) => {
+		e.preventDefault();
+		createProfile(formData, history)
+	}
+
 
 	const [displaySocialInputs, toggleSocialInputs] = useState(false)
 
@@ -51,7 +57,7 @@ const CreateProfile = (props) => {
 				profile stand out
 			</p>
 			<small>* = required field</small>
-			<form className='form'>
+			<form className='form' onSubmit={(e) => onSubmit(e)}>
 				<div className='form-group'>
 					<select name='status' value={status} onChange={e => onChange(e)}>
 						<option value='0'>* Select Professional Status</option>
@@ -211,4 +217,9 @@ const CreateProfile = (props) => {
 	);
 }
 
-export default connect()(CreateProfile);
+
+
+export default connect(
+	null,
+	{createProfile}
+)(withRouter(CreateProfile));
