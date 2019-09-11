08f72fd7e3499947a701cb568e06ffd41eceecfb
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-09-11 22:34:57 +0300

    profile = ProfileExperience, ProfileEducation

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import Spinner from '../layout/Spinner'
 import {Link} from "react-router-dom";
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
+import ProfileEducation from './ProfileEducation'
 import {getProfileById} from "../../actions/profile";
 
 const Profile = ({
@@ -28,7 +30,28 @@ const Profile = ({
 					<div className="profile-grid my-1">
 						<ProfileTop profile={profile}/>
 						<ProfileAbout profile={profile}/>
+						<div className="profile-exp bg-white p-2">
+							<h2 className="text-primary">Experience</h2>
+							{profile.experience.length > 0 ? (
+								<>
+									{profile.experience.map((experience) => {
+										return <ProfileExperience key={experience._id} experience={experience}/>
+									})}
+								</>
+							) : (<h4>No experience</h4>)}
+						</div>
+						<div className="profile-edu bg-white p-2">
+							<h2 className="text-primary">Education</h2>
+							{profile.education.length > 0 ? (
+								<>
+									{profile.education.map((education) => {
+										return <ProfileEducation key={education._id} experience={education}/>
+									})}
+								</>
+							) : (<h4>No education</h4>)}
+						</div>
 					</div>
+
 				</>
 			)}
 		</>
