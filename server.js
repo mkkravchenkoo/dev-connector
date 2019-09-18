7034a89dbb0771cd3b315d3f73ae73492113b6db
Nikolay Kravchenko <mkkravchenkoo@gmail.com>
2019-09-18 20:09:00 +0300

    1. heroku login 2. heroku create 3. heroku git:remote -a aqueous-dusk-17650 Preparing to deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
 
@@ -21,9 +22,18 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-app.get('/', (req, res) => {
-	res.send('API running')
-});
+if(process.env.NODE_ENV === 'production'){
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+
+}
+
+// app.get('/', (req, res) => {
+// 	res.send('API running')
+// });
 
 const PORT = process.env.PORT || 5000;
 
